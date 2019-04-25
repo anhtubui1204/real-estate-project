92f9df5841d369ae5bbb5ceeee2bd6df7b9379b8
AnhTu <anhtu.bui120491@gmail.com>
2019-04-25 14:56:25 +0700

    hi
    hi

--- routes/api/ads.js
+++ routes/api/ads.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const router = express.Router();
 
+//@ route GET api/ads/test
+//Test ads route
 router.get('/test',(req, res)=> {
     res.json({
         msg: "ads works"
