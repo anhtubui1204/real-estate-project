30b8f22f68b2ada10fe7d01315997dc8a1ca7f8c
ezioazai <anhtu.bui120491@gmail.com>
2019-04-27 09:57:13 +0700

    hi
    hi

--- models/Ads.js
+++ models/Ads.js
@@ -7,5 +7,73 @@ const AdsSchema = new Schema({
     user: {
         type: Schema.Types.ObjectId,
         ref: 'users'
+    },
+    title: {
+        type: String,
+        required: true
+    },
+    price: {
+        type: Number,
+        required: true
+    },
+    area: {
+        type: Number,
+        required: true
+    },
+    nBedRooms: {
+        type: Number,
+        required: true
+    },
+    nFloors: {
+        type: Number,
+        required: true
+    },
+    direction: {
+        type: String
+    },
+    address: {
+        street: {
+            type: 'String'
+        },
+        district: {
+            type: 'String'
+        },
+        city: {
+            type: 'String'
+        }
+        
+    },
+    contactInfo: {
+        name: {
+            type: String,
+            required: true
+        },
+        phone: {
+            type: Number,
+            required: true
+        }
+    },
+    postDate: {
+        type: Date,
+        default: Date.now
+    },
+    imageURL: {
+        imageMain: {
+            type: 'String',
+            required: true
+        },
+        otherImgages: {
+            image1: {
+                type: 'String'
+            },
+            image2: {
+                type: 'String'
+            },
+            image3: {
+                type: 'String'
+            }
+        }
     }
 })
+
+module.exports = Ads = mongoose.model('ads', AdsSchema);
