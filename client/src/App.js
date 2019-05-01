1f190538a12570afaba8ff251cb954807c52e472
AnhTu <anhtu.bui120491@gmail.com>
2019-05-01 15:31:38 +0700

    hi
    hi

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { BrowserRouter as Router} from "react-router-dom";
 
 import AppNavbar1 from './components/layout/AppNavbar1';
+import LoginLogout from './components/Users/LoginLogout';
 
 class App extends Component {
   render() {
@@ -10,6 +11,7 @@ class App extends Component {
         <div className="App">
           <AppNavbar1/>
           <h1>hello</h1>
+          <LoginLogout/>
         </div>
       </Router>
     );
